2b0dcaf082a45dd9583a8d0e10391c958ce99e3e
zygyy <1297799065@qq.com>
2019-12-29 16:30:51 +0800

    登录功能的完成

--- src/router/index.js
+++ src/router/index.js
@@ -5,33 +5,33 @@ import Home from '@/components/Home'
 
 Vue.use(Router)
 
-const router=new Router({
+const router = new Router({
   routes: [
     {
-      path:'/',
-      component:Login
+      path: '/',
+      component: Login
     },
     {
-      path:'/login',
-      component:Login
+      path: '/login',
+      component: Login
     },
     {
-      path:'/home',
-      component:Home
+      path: '/home',
+      component: Home
     },
   ]
 })
 //挂载路由导航守卫
-router.beforeEach((to,from,next)=>{
-
-  if(to.path==='/login'){
+router.beforeEach((to, from, next) => {
+  //to将要访问的路径,from代表从哪个路径跳转而来,next()是一个函数,表示放行
+  if (to.path === '/login') {
     return next();
-  }else{
+  } else {
     //先获取token
-    const token=window.sessionStorage.getItem('token');
-    if(!token){
+    const token = window.sessionStorage.getItem('token');
+    if (!token) {
       return next('/login');
-    }else{
+    } else {
       next();
     }
   }
