5a57293355c96cb1ad834edf825bcb3b6dc8862b
zygyy <1297799065@qq.com>
2019-12-29 20:11:23 +0800

    主界面的顶部和左侧导航栏的编写以及为请求头添加token验证

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,20 +1,111 @@
 <template>
-    <div>
-      <el-button type="info" @click="logout">退出</el-button>
-    </div>
+  <div>
+    <el-container class="home-container">
+      <!-- 头部 -->
+      <el-header>
+        <div>
+          <img src="../assets/heima.png" />
+          <span>电商后台管理系统</span>
+        </div>
+        <el-button type="info" @click="logout">退出</el-button>
+      </el-header>
+      <el-container>
+        <!-- 左侧导航栏 -->
+        <el-aside width="200px">
+          <el-menu background-color="#333744" text-color="#fff" active-text-color="#409EFF">
+            <!-- 一级菜单 -->
+            <el-submenu :index="item.id+''" v-for="item in menulist" :key="item.id">
+              <template slot="title">
+                <i :class="iconsOBJ[item.id]"></i>
+                <span>{{item.authName}}</span>
+              </template>
+
+              <!-- 二级菜单 -->
+              <el-menu-item
+                :index="subItem.id+''"
+                v-for="subItem in item.children"
+                :key="subItem.id"
+              >
+                <template slot="title">
+                  <i class="el-icon-menu"></i>
+                  <span>{{subItem.authName}}</span>
+                </template>
+              </el-menu-item>
+            </el-submenu>
+          </el-menu>
+        </el-aside>
+        <!-- 右侧主体区 -->
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
+  </div>
 </template>
+
 <script>
 export default {
-    methods:{
-        logout(){
-            window.sessionStorage.clear();
-            this.$router.push('/login');
-        }
+  data() {
+    return {
+      //左侧菜单数据
+      menulist: [],
+      iconsOBJ: {
+        "125": "iconfont icon-user",
+        "103": "iconfont icon-tijikongjian",
+        "101": "iconfont icon-shangpin",
+        "102": "iconfont icon-danju",
+        "145": "iconfont icon-baobiao"
+      }
+    };
+  },
+  created() {
+    this.getMenuList();
+  },
+  methods: {
+    logout() {
+      window.sessionStorage.clear();
+      this.$router.push("/login");
+    },
+    //获取所有菜单
+    async getMenuList() {
+      const { data: result } = await this.$http.get("menus");
+      if (result.meta.status !== 200) {
+        return this.$message.error(result.meta.msg);
+      } else {
+        this.menulist = result.data;
+      }
     }
-    
-}
+  }
+};
 </script>
-<style scoped>
-
-
+<style lang='less' scoped>
+/* 类选择器 */
+.home-container {
+  height: 801px;
+  margin: 0;
+  padding: 0;
+}
+.el-header {
+  background-color: #373d41;
+  display: flex;
+  justify-content: space-between;
+  padding-left: 0;
+  align-items: center;
+  color: #fff;
+  font-size: 20px;
+  > div {
+    display: flex;
+    align-items: center;
+    span {
+      margin-left: 15px;
+    }
+  }
+}
+.el-aside {
+  background-color: #333744;
+}
+.el-main {
+  background-color: #eaedf1;
+}
+.iconfont {
+  margin-right: 10px;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -10,6 +10,13 @@ import axios from 'axios'
 
 //配置请求根路径
 axios.defaults.baseURL='http://127.0.0.1:8888/api/private/v1/'
+// 通过axios1请求拦截器添加token，保证拥有获取数据的权限
+axios.interceptors.request.use(config=>{
+  //console.log(config);
+  //为请求头对象，添加token验证的Authorization字段
+  config.headers.Authorization=window.sessionStorage.getItem('token')
+  return config;
+})
 Vue.prototype.$http=axios
 Vue.use(ElementUI);
 Vue.config.productionTip = false
